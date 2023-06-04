a64d9defc9d203b84d24eca570011595a0a45d4a
Crowdbotics <team@crowdbotics.com>
2023-06-04 11:59:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dawn_fire_41230/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dawn_fire_41230;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dawn_fire_41230";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dawn-fire-41230",
+  url: "https://dawn-fire-41230.botics.co",
+  api: "https://dawn-fire-41230.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
